d09bd1b298e6792eabb5ca59466ecc80dde94103
= <=>
2025-10-23 09:50:00 -0400

    chore: update Lume and add test route

--- _config.ts
+++ _config.ts
@@ -1,8 +1,14 @@
 import lume from "lume/mod.ts";
+import router from "./src/routes.ts";
 import plugins from "./plugins.ts";
 
 const site = lume({
   src: "./src",
+  server: {
+    middlewares: [
+      router.middleware()
+    ],
+  },
 });
 
 site.use(plugins());
---
+++ src/routes.ts
@@ -0,0 +1,27 @@
+import Router from "lume/middlewares/router.ts";
+
+const _DEV_MODE = Deno.env.get("LUME_DRAFTS") || false;
+const router = new Router();
+
+/* Filter out XSS attempts */
+function _sanitizeString(str: string): string {
+  if (!str) {
+    return "";
+  }
+  return str
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&apos;");
+}
+
+/* Define routes for your site here */
+
+// The simple datetime server
+router.get("/api", ({ _request }) => {
+  const ts = Date.now();
+  return new Response(JSON.stringify({ time: ts }), { status: 200 });
+});
+
+export default router;
