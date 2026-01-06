0051ed99630335e0a3ca1732b10e94a30d0efe2f
= <=>
2026-01-06 14:02:22 -0500

    Just use the class. New store.

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import Router from "lume/middlewares/router.ts";
-import { Libsplinter } from "./splinter_ffi.ts";
+import { Splinter } from "./splinter.class.ts";
+
 const router = new Router();
 
 /* Filter out XSS attempts */
@@ -32,34 +33,10 @@ router.get("/api", ({ _request }) => {
 });
 
 router.get("/splinter-check", ({ _request }) => {
-
-  if (Libsplinter.symbols.splinter_open(<BufferSource> test_bus) !== 0) {
-    return new Response(JSON.stringify({error: "Could not open store " + temp_path }), { status: 500 });
-  }
-  
-  const [keyBuf] = cstr("hello");
-  const valBuf = new Uint8Array(new TextEncoder().encode("from Deno!"));
-  const valPtr = Deno.UnsafePointer.of(valBuf);
-
-  if (Libsplinter.symbols.splinter_set(<BufferSource> keyBuf, valPtr, BigInt(valBuf.byteLength)) !== 0) {
-    return  new Response(JSON.stringify({ error: "splinter_set"}), { status: 500 });
-  }
-
-  const out = new Uint8Array(128);
-  const outPtr = Deno.UnsafePointer.of(out);
-  const outLen = new Uint32Array(1);
-  const outLenPtr = Deno.UnsafePointer.of(outLen);
-  
-  if (
-    Libsplinter.symbols.splinter_get(<BufferSource> keyBuf, outPtr, BigInt(out.byteLength), outLenPtr) !== 0
-  ) {
-    return new Response(JSON.stringify({ error: "splinter_get"}), { status: 500 });
-  }
-  
-  const result = new TextDecoder().decode(out.subarray(0, outLen[0]));
-  Libsplinter.symbols.splinter_close();
-  
-  return new Response(JSON.stringify({ contents: result }), { status: 200});
+  const conn = Splinter.open(temp_path);
+  conn.set("test_key", "test value");
+  const val = conn.getString("test_key");
+  return new Response(JSON.stringify({ value: val }), { status: 200 });
 });
 
 export default router;
